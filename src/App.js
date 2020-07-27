294409d4d5e7bb9561211c27fe726181256ab44b
Mohammed Imran <mohammedimran86992@gmail.com>
2020-07-27 20:32:25 +0530

    :sparkles: Build layouts were mishandled, rectified.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import { API_KEY, STATE_WISE_API_KEY } from './env.js';
-// import Stat from './Components/Stat.js';
 import loading from './assets/loading.gif';
 import Cards from './Components/Cards/Cards'
 import DistrictSearch from './Components/DistrictSearch/DistrictSearch'
@@ -11,7 +10,7 @@ function App() {
   const [covidData, setCovidData] = useState({
     districts: [],
     isLoading: true,
-  })
+  });
 
   const [districtData, setDistrictData] = useState({
     data: {},
@@ -21,7 +20,7 @@ function App() {
   const handleDistrictChange = (district) => {
     const data = covidData.districts;
     const parsedData = data.filter(dist => {
-      return dist[district]
+      return dist[district];
     })
     setDistrictData({ data: parsedData[0], district: district });
   }
@@ -39,19 +38,19 @@ function App() {
         let districtWiseV2 = dataV2.Karnataka.districtData;
 
         let districts = [];
-        let active = []
+        let active = [];
 
         let eachDistrictV1 = Object.keys(districtWiseV1);
         let eachDistrictV2 = Object.keys(districtWiseV2);
 
         eachDistrictV2.forEach(dist => {
-          active.push(districtWiseV2[dist].active)
+          active.push(districtWiseV2[dist].active);
         });
 
         eachDistrictV1.forEach((dist, index) => {
-          let obj = {}
+          let obj = {};
           obj[dist] = districtWiseV1[dist];
-          obj[dist].active = active[index]
+          obj[dist].active = active[index];
           districts.push(obj);
         });
 
@@ -61,8 +60,8 @@ function App() {
     }
     fetchData();
   }, []);
-  let distValues = []
-  // console.log(districtData.data)
+  let distValues = [];
+
   const changingView = () => {
     if (districtData.district.length <= 1) return;
 
@@ -72,21 +71,24 @@ function App() {
     let { confirmed, deceased, recovered } = total;
 
     distValues = [confirmed, active, recovered, deceased, lastUpdated]
-    console.log(confirmed, active, recovered, deceased, lastUpdated)
   }
   changingView();
 
-  // const dummyData = [130000, 70000, 50000, 2000]
   return (
     <div className="App">
       {covidData.isLoading ? <img src={loading} alt="loading gif" /> :
         <>
-          <h1>{districtData.district}</h1>
-          <DistrictSearch className={DistrictSearch} data={covidData.districts} handleDistrictChange={handleDistrictChange} />
-          <Cards className={Cards} data={distValues} />
-          <Chart className={Chart} data={distValues} />
+          <h1 id='districtName'>{districtData.district || "Select a Districts."}</h1>
+          <div className='district-search'>
+            <DistrictSearch className={DistrictSearch} data={covidData.districts} handleDistrictChange={handleDistrictChange} />
+          </div>
+          <div className='cards'>
+            <Cards className={Cards} data={distValues} />
+          </div>
+          <div className='chart'>
+            <Chart className={Chart} data={distValues} />
+          </div>
         </>
-
       }
     </div>
   );
