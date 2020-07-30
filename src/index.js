2c1d00d55905d96c29c108d21e3a609f90ac98e1
Mohammed Imran <mohammedimran86992@gmail.com>
2020-07-30 18:52:23 +0530

    :sparkles: footer tag was modified.
    Signed-off-by: Mohammed Imran <mohammedimran86992@gmail.com>

--- src/index.js
+++ src/index.js
@@ -8,8 +8,10 @@ ReactDOM.render(
   <React.Fragment>
     <App />
     <footer id="footer">
-      <p>Made with <span role='img'>💞</span> by <a href="https://www.instagram.com/m0hd_imran/" target="_black">Mohammed Imran</a></p>
-      <a id="github-logo" href="https://github.com/M0hammedImran/covid19-india" target="_blank"><img id="github-logo" src={githubLogo} alt="github logo" /></a>
+      <p>Made with <span aria-label="heart emoji" role='img'>💞</span> by <a href="https://www.instagram.com/m0hd_imran/" target="_black">Mohammed Imran</a></p>
+      <a id="github-logo" href="https://github.com/M0hammedImran/covid19-india" rel="noopener noreferrer" target="_blank">
+        <img id="github-logo" src={githubLogo} alt="github logo" />
+      </a>
     </footer>
   </React.Fragment>,
   document.getElementById('root')
